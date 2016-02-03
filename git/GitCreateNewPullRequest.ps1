ffe95e82bd7b89a443c3985e1d36b62e0ba92089
Nathan Johnstone <njohnstone@boardbooks.com>
2016-02-03 14:43:53 +1300

    fixes to script to handle when environment variable is not defined

--- git/GitCreateNewPullRequest.ps1
+++ git/GitCreateNewPullRequest.ps1
@@ -17,32 +17,36 @@ $origin = git config --get remote.origin.url
 # Remove the .git from the end if present
 $origin = $origin.Replace(".git","") 
 
+# Get the default target repo for the pull request
+$targetRepo = [Environment]::GetEnvironmentVariable("DefaultGitPullRequestRepo","User")
+
+# Get the default target branch for pull requests
+$defaultTargetBranch = [Environment]::GetEnvironmentVariable("DefaultGitPullRequestBranch","User")
+
 # Check for a hyphen in the current branch and assume that the left part is the target branch
 if ($branch -like "*-*")
 {
-    $targetBranch = "Development:" + $branch.Split("-")[0];
+    $targetBranch = $targetRepo + ":" + $branch.Split("-")[0];
 }
-else
+elseif ($defaultTargetBranch)
 {
     # Otherwise use the default git pull request branch from the environment variable
-    $targetBranch = [Environment]::GetEnvironmentVariable("DefaultGitPullRequestBranch","User")
+    $targetBranch = $defaultTargetBranch
 }
-
-# No target branch, try raising pull request to a matching branch name against the default repo
-if ($targetBranch -match "") 
+# No target branch, try raising pull request to a matching current branch name against the default repo
+else
 {
-    $remoteRepo = [Environment]::GetEnvironmentVariable("DefaultGitPullRequestRepo","User")
-    $targetBranch = $remoteRepo + ":" + $branch
+    $targetBranch = $targetRepo + ":" + $branch
 }
 
 # Create the pull request URL 
-If($targetBranch -notmatch $null)
+If($targetBranch)
 {
-	$url = $origin + "/compare/" + $branch + "?expand=1"
+    $url = $origin + "/compare/" + $targetBranch + "..." + $branch + "?expand=1"	
 }
 else
 {
-	$url = $origin + "/compare/" + $targetBranch + "..." + $branch + "?expand=1"
+	$url = $origin + "/compare/" + $branch + "?expand=1"
 }
 
 Start $url
